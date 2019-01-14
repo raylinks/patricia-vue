a63aa10097d06f889bde21adaf832caa53f910d9
raybaba <olawuyirilwan99@gmail.com>
2019-01-14 07:48:13 +0000

    'commit_messkaag'

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-export const apiDomain = 'http://teamltd.org/';
+export const apiDomain = 'http://localhost:3000/';
 export const forgotpassword = `${apiDomain}api/v1/users/forget`;
 export const resetpassword = `${apiDomain}api/v1/users/reset`;
 export const login = `${apiDomain}api/v1/users/login`;
